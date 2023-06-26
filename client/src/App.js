2904f059a7e0e87ab39188d09eabead1dbfd9669
kimgyul11 <coloo88@naver.com>
2023-06-26 09:36:02 +0900

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const SERVER_URL = "http://localhost:4000/api/todo";
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get(SERVER_URL);
+    setTodoList(response.data);
+    // fetch("http://localhost:4000/api/todo")
+    //   .then((res) => res.json())
+    //   .then((data) => setTodoList(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    const done = e.target.done.checked;
+    await axios.post(SERVER_URL, { text, done });
+    fetchData();
+    // fetch("http://localhost:4000/api/todo", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     text,
+    //     done,
+    //   }),
+    // }).then(() => {
+    //   fetchData();
+    // });
+  };
   return (
     <div>
       <h1>투두 리스트</h1>
+      <form onSubmit={onSubmitHandler}>
+        <input name="text" />
+        <input name="done" type="checkbox" />
+        <input type="submit" value="추가" />
+      </form>
+      {todoList?.map((todo) => (
+        <div key={todo.id} style={{ display: "flex" }}>
+          <div>{todo.id}</div>
+          <div>{todo.text}</div>
+          <div>{todo.done ? "Y" : "N"}</div>
+        </div>
+      ))}
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.use(cors());
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+let id = 2;
+const todoList = [
+  {
+    id: 1,
+    text: "할일 1",
+    done: false,
+  },
+];
+
+app.get("/api/todo", (req, res) => {
+  res.json(todoList);
 });
 
-app.listen(3000, () => {
+app.post("/api/todo", (req, res) => {
+  const { text, done } = req.body;
+  todoList.push({
+    id: id++,
+    text,
+    done,
+  });
+  return res.send("success");
+});
+
+app.listen(4000, () => {
   console.log("server start");
 });
